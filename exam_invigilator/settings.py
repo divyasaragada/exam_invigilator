1bbfe701342d2bc913d8927cd7961cdd67c10325
divyasaragada <58907293+divyasaragada@users.noreply.github.com>
2020-09-15 20:37:55 +0530

    Update settings.py

--- exam_invigilator/settings.py
+++ exam_invigilator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0)esn8%*$tws9^p=@xbo*p)&(3vjq+m*ovw(lj*so=01172zpl'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
